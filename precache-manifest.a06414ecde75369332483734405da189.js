7587f8c45ece91f439952a6e86b6042cb27d507f
pardeshiamol <amolpardeshi074@gmail.com>
2021-12-22 10:35:05 +0530

    Updates

---
+++ precache-manifest.a06414ecde75369332483734405da189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a99e136034ce10f6635357d0f6bd329",
+    "url": "/TextUtills1/index.html"
+  },
+  {
+    "revision": "30fa1b134bce93b08814",
+    "url": "/TextUtills1/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "501fe35a60520225eb49",
+    "url": "/TextUtills1/static/js/2.19b5bf68.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/TextUtills1/static/js/2.19b5bf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8692a46e09dbf89e03ed",
+    "url": "/TextUtills1/static/js/3.8068ac1e.chunk.js"
+  },
+  {
+    "revision": "30fa1b134bce93b08814",
+    "url": "/TextUtills1/static/js/main.5b0e2d33.chunk.js"
+  },
+  {
+    "revision": "dabb30a325f000729ba9",
+    "url": "/TextUtills1/static/js/runtime-main.722a6cf8.js"
+  }
+]);
